97fbf18274550ebb3debcfbce7a8b321b137b925
nicholasneo78 <nicholasneo78@gmail.com>
2022-04-01 16:23:33 +0800

    added more preprocessing code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,5 @@
 # imports
+from distutils.command.clean import clean
 import os
 from os.path import join
 import numpy as np
@@ -11,6 +12,8 @@ from tqdm import tqdm
 import pickle
 from datasets import Dataset, DatasetDict
 import re
+from num2words import num2words
+from nltk import flatten
 
 # generate the csv with all the data required to build the DatasetDict for the finetuning step 
 class GeneratePickle():
@@ -39,7 +42,67 @@ class GeneratePickle():
 
         df_new = pd.DataFrame(split_list_frame, columns=['id', 'annotations']) # id and annotations are just dummy headers here
         return df_new 
-    
+
+
+    # to input the text and detect if any digits exists, if there is, will convert the numbers into its word representation
+    def get_text_from_number(self, text):
+        # split sentence to list of words
+        text_list = text.split()
+        new_text_list = []
+        
+        for txt in text_list:
+            
+            # check if word is STRICTLY alphanumeric, not either one of it
+            if (txt.isalnum()) and (not txt.isalpha()) and (not txt.isnumeric()):
+                sep_alpha_numeric_list = []
+                
+                # iterate at the letter/digits level
+                for letter_idx, letter in enumerate(list(txt)):
+                    
+                    # append original letter
+                    sep_alpha_numeric_list.append(txt[letter_idx])
+                    # print(sep_alpha_numeric_list)
+                    # print(len(list(txt)))
+                    # print(letter_idx)
+                    
+                    # compare the current indexed letter/digits and the next index letter/digits
+                    if letter_idx != len(list(txt))-1 and ((txt[letter_idx].isalpha() and txt[letter_idx+1].isnumeric()) or (txt[letter_idx].isnumeric() and txt[letter_idx+1].isalpha())):                    
+                        sep_alpha_numeric_list.append('%')
+                
+                # join the list of characters to a word again but with '%' separator
+                new_text_list.append(''.join(sep_alpha_numeric_list))
+                
+            # if word is not STRICTLY alphanumeric, just append the word
+            else:
+                new_text_list.append(txt)
+        
+        # remove the separator '%'
+        preprocessed_text = ' '.join(flatten(new_text_list)).replace('%', ' ')
+        
+        # split the text into list of words again
+        preprocessed_text_list = preprocessed_text.split()
+        
+        # print(preprocessed_text_list)
+        
+        # check and see if the individual strings are digits or not
+        for idx, t in enumerate(preprocessed_text_list):
+            try:
+                # if less than 100, will pronounced in the usual way => e.g 55 == fifty-five
+                if float(t) <= 100:
+                    preprocessed_text_list[idx] = num2words(t)
+                # else pronounced by its individual number => e.g 119 => one one nine
+                else:
+                    sep_num_list = []
+                    for k in list(t):
+                        sep_num_list.append(num2words(k))
+                    preprocessed_text_list[idx] = sep_num_list
+            except:
+                continue
+                
+        text_list_flat = flatten(preprocessed_text_list)
+        return ' '.join(text_list_flat).upper()
+
+
     # generate the csv to prepare the dataset for the finetuning step
     def build_pickle(self):
         # list to append all the data in
@@ -63,13 +126,21 @@ class GeneratePickle():
                         # the sub dictionary for the audio component 
                         # the main dictionary which comprises the file, audio and text component
                     
-                    # text preprocessing
+                    ## TEXT-PREPROCESSING ##
+
+                    # replace filler words with one symbol: #
                     clean_text = df_new.loc[df_new['id'] == base_path, 'annotations'].to_numpy()[0].replace('#', ' ').replace('<FIL>', '#').replace('<FILL>', '#')
-                    clean_text = re.sub(r'[^A-Za-z0-9#-\' ]+', ' ', clean_text)
+                    
+                    # keep only certain characters
+                    clean_text = re.sub(r'[^A-Za-z0-9#\' ]+', ' ', clean_text)
+
+                    # convert all the digits to its text equivalent
+                    clean_text = self.get_text_from_number(clean_text)
+
                     # convert multiple spaces into only one space
                     clean_text = ' '.join(clean_text.split())
 
-                    # creating the dictionary
+                    # creating the final data dictionary that is to be saved to a pkl file
                     data = {
                         'file': os.path.join(root, file),
                         'audio': {
