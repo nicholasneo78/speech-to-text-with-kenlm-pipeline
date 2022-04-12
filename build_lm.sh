13b4fb8ab5e4a124285574fc88594c220293cfad
nicholasneo78 <nicholasneo78@gmail.com>
2022-04-12 11:47:09 +0800

    minor edits to bash script

--- build_lm.sh
+++ build_lm.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 n_gram="5"
 dataset_name="magister"
-txtfile_loc="lm/magister_text_build_lm.txt"
+txtfile_loc="lm/magister_annotations.txt"
 
-output_arpa_loc="lm/${n_gram}gram_${dataset_name}.arpa"
+output_arpa_loc="lm/${n_gram}_gram_${dataset_name}.arpa"
 
 # script execution
 /workspace/kenlm/build/bin/lmplz -o $n_gram < $txtfile_loc > $output_arpa_loc
-
-# script execution
-#/workspace/kenlm/build/bin/lmplz -o 5 <lm_model_prep.txt >magister_lm.arpa
