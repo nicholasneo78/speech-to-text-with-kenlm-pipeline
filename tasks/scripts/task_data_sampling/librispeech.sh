ebcc73553d04b173ff7d1e16d5c6e6039c2699ed
nicholasneo78 <nicholasneo78@gmail.com>
2022-05-16 22:06:49 +0800

    refactor code

--- tasks/scripts/task_data_sampling/librispeech.sh
+++ tasks/scripts/task_data_sampling/librispeech.sh
@@ -7,7 +7,7 @@ python3 ../../task_data_sampling.py \
     --dataset_name "librispeech_v2_data_sampling" \
     --output_url "s3://experiment-logging" \
     --dataset_project "datasets/librispeech" \
-    --dataset_task_id "fdb1e1471ebb4b8dbf4f599080401819" \
+    --dataset_task_id ["fdb1e1471ebb4b8dbf4f599080401819", "fdb1e1471ebb4b8dbf4f599080401819"] \
     --input_train_dict {"pkl/librispeech_train.pkl": 0.5, "pkl/librispeech_train.pkl": 0.2} \
     --input_dev_dict {"pkl/librispeech_dev.pkl": 1, "pkl/librispeech_dev.pkl": 1} \
     --input_test_dict {"pkl/librispeech_test.pkl": 1, "pkl/librispeech_test.pkl": 1} \
--- tasks/task_data_sampling.py
+++ tasks/task_data_sampling.py
@@ -17,7 +17,7 @@ def parse_args():
     parser.add_argument("--dataset_project",              type=str, help="the clearml path which the datasets resides")
     
     # arguments correspoding to the data_preprocessing.py file
-    parser.add_argument("--dataset_task_id",              type=str, help="task id to retrieve the dataset")
+    parser.add_argument("--dataset_task_id",              type=list, help="task ids list to retrieve the datasets, if there are multiple datasets")
     parser.add_argument("--input_train_dict",             type=dict, help="path to the input train pickle file")
     parser.add_argument("--input_dev_dict",               type=dict, help="path to the input dev pickle file")
     parser.add_argument("--input_test_dict",              type=dict, help="path to the input test pickle file")
@@ -26,8 +26,7 @@ def parse_args():
     parser.add_argument("--output_pkl_test",              type=str, help="path to produce the test pickle file")
     parser.add_argument("--sampling_mode",                type=str, help="sample either the manual way or the least way")
     parser.add_argument("--random_state",                 type=int, help="random seed")
-
-
+    
     # queue name
     parser.add_argument("--queue",                        type=str, help="the queue name for clearml")   
 
@@ -45,28 +44,51 @@ task.execute_remotely(queue_name=arg.queue, exit_process=True)
 
 from preprocessing.data_sampling import DataSampling
 
-# obtain the pkl files
-dataset_pkl = Dataset.get(dataset_id=arg.dataset_task_id)
-dataset_pkl_path = dataset_pkl.get_local_copy()
-
-# register clearml sampled pkl file dataset
+# register clearml sampled combined train pkl file dataset, together with the dev and test dataset 
 dataset = Dataset.create(
     dataset_project=arg.dataset_project,
     dataset_name=arg.dataset_name
 )
 
+# obtain the pkl files
+dataset_pkl_path_list = []
+
+for i, j in enumerate(arg.dataset_task_id):
+    dataset_pkl = Dataset.get(dataset_id=j)
+    dataset_pkl_path = dataset_pkl.get_local_copy()
+    dataset_pkl_path_list.append(dataset_pkl_path)
+
+
+def preprocess_dictionary(input_dict: dict, dataset_pkl_path_list: list) -> dict:
+    '''
+        function to preprocess the input dictionary, by appending the correct clearml filepath to the key of the input dictionary
+    '''
+
+    idx = 0
+    output_dict = {}
+
+    # append clearml filepath to the key iteration    
+    for key in input_dict:
+        output_dict[f'{dataset_pkl_path_list[idx]}/{key}'] = input_dict[key]
+        idx+=1
+
+    return output_dict
+
 # proceed with the sampling
-pkl_train = DataSampling(data_dict=f'{dataset_pkl_path}/{arg.input_train_dict}',
+pkl_train = DataSampling(data_dict=preprocess_dictionary(input_dict=arg.input_train_dict, 
+                                                         dataset_pkl_path_list=dataset_pkl_path_list),
                          sampling_mode=arg.sampling_mode, 
                          final_pkl_location=arg.output_pkl_train, 
                          random_state=arg.random_state)
 
-pkl_dev = DataSampling(data_dict=f'{dataset_pkl_path}/{arg.input_dev_dict}',
+pkl_dev = DataSampling(data_dict=preprocess_dictionary(input_dict=arg.input_dev_dict, 
+                                                       dataset_pkl_path_list=dataset_pkl_path_list),
                        sampling_mode=arg.sampling_mode, 
                        final_pkl_location=arg.output_pkl_dev, 
                        random_state=arg.random_state)
 
-pkl_test = DataSampling(data_dict=f'{dataset_pkl_path}/{arg.input_test_dict}',
+pkl_test = DataSampling(data_dict=preprocess_dictionary(input_dict=arg.input_test_dict, 
+                                                        dataset_pkl_path_list=dataset_pkl_path_list),
                         sampling_mode=arg.sampling_mode, 
                         final_pkl_location=arg.output_pkl_test, 
                         random_state=arg.random_state)
